5d7f7b8a3865a73c88b7d3761b7e80cba40d6763
annagao <ggaoyanyan@gmail.com>
2021-10-20 10:57:11 -0700

    Uncontrolled Form

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -3,37 +3,33 @@ import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem } from 'r
 import { Link } from 'react-router-dom';
 
 
-  
-
 function RenderCampsite({campsite}){
-   
-       return(   
-       <div className="col-md-5 m-1" >     
+    return(   
+        <div className="col-md-5 m-1" >     
         <Card>
-          <CardImg top src={campsite.image} alt={campsite.name} />
-             <CardBody>
+            <CardImg top src={campsite.image} alt={campsite.name} />
+                <CardBody>
                 <CardText>{campsite.description}</CardText>
-             </CardBody>
-         </Card>
+                </CardBody>
+            </Card>
         </div>      
-             )
+        )
     }
 
 function RenderComments({comments}){
-   
     if(comments){
         return(
             <div className="col-md-5 m-1">
-               <h4>Comments</h4>
-               {comments.map(comment =>
-                 <div key={comment.id}>
-                  {comment.text} <br/>
-                  {'-- '+comment.author+", "}
-                  {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
-                  <br/><br/>
-               </div>
-               )
-               }
+                <h4>Comments</h4>
+                {comments.map(comment =>
+                <div key={comment.id}>
+                    {comment.text} <br/>
+                    {'-- '+comment.author+", "}
+                    {new Intl.DateTimeFormat('en-US', { year: 'numeric', month: 'short', day: '2-digit'}).format(new Date(Date.parse(comment.date)))}
+                    <br/><br/>
+            </div>
+        )
+    }
 
             </div>
         )
@@ -55,17 +51,15 @@ function CampsiteInfo(props){
                         <h2>{props.campsite.name}</h2>
                     </div>
                     </div>
-                 <div className="row"> 
-                     <RenderCampsite campsite={props.campsite} />
-                     <RenderComments comments={props.comments} />
-                 </div>
-                 </div>
+                    <div className="row"> 
+                        <RenderCampsite campsite={props.campsite} />
+                        <RenderComments comments={props.comments} />
+                </div>
+            </div>
             )
         }
             return (<div></div>)
         
-}
-   
-            
+}        
 
     export default CampsiteInfo;
