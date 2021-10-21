228b73ed763237ca4a33b2e98c90759111530de4
annagao <ggaoyanyan@gmail.com>
2021-10-21 15:24:42 -0700

    React Redux Form

--- src/App.js
+++ src/App.js
@@ -1,16 +1,29 @@
 import React, { Component } from 'react';
 import Main from './components/MainComponent';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import { ConfiguresStore } from './redux/configuresStore';
 import './App.css';
 
+
+const store = ConfiguresStore();
+<Provider store={store}>
+    <BrowserRouter>
+        <div className="App">
+            <Main/>
+        </div>
+    </BrowserRouter>
+</Provider>
 class App extends Component {
     render() {
         return ( 
-            <BrowserRouter >
-                  <div className = "App" >
-                      <Main />
-                  </div>
-            </BrowserRouter>  
+            <Provider store={store}>
+                <BrowserRouter>
+                    <div className="App">
+                        <Main/>
+                    </div>
+                </BrowserRouter>
+            </Provider>  
         );
     };
 }
