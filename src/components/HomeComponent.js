37ce228793cad3cc019213b32af5b275bc6eab98
annagao <ggaoyanyan@gmail.com>
2021-12-05 21:55:26 -0800

    Fetch from Server

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,6 +1,27 @@
 import React from 'react';
 import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
 import { Loading } from './LoadingComponent';
+import { baseUrl } from '../shared/baseUrl';
+
+
+
+function RenderCard({item, isLoading, errMess}) {
+    if (isLoading) {
+        return <Loading />;
+    }
+    if (errMess) {
+        return <h4>{errMess}</h4>;
+    }
+    return (
+        <Card>
+            <CardImg src={baseUrl + item.image} alt={item.name} />
+            <CardBody>
+                <CardTitle>{item.name}</CardTitle>
+                <CardText>{item.description}</CardText>
+            </CardBody>
+        </Card>
+    );
+}
 
 function Home(props) {
     return (
@@ -12,7 +33,9 @@ function Home(props) {
                                 errMess={props.campsitesErrMess}/>
                 </div>
                 <div className="col-md m-1">
-                    <RenderCard item={props.promotion} />
+                    <RenderCard item={props.promotion}
+                                    isLoading={props.promotionLoading}
+                                    errMess={props.promotionErrMess} />
                 </div>
                 <div className="col-md m-1">
                     <RenderCard item={props.partner} />
@@ -21,25 +44,5 @@ function Home(props) {
         </div>
     );
 }
-
-function RenderCard({item, isLoading, errMess}) {
-    if (isLoading) {
-        return <Loading />;
-    }
-    if (errMess) {
-        return <h4>{errMess}</h4>;
-    }
-    return (
-        <Card>
-            <CardImg src={item.image} alt={item.name} />
-            <CardBody>
-                <CardTitle>{item.name}</CardTitle>
-                <CardText>{item.description}</CardText>
-            </CardBody>
-        </Card>
-    );
-}
-
-
 
 export default Home;
